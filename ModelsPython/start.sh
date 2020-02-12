b7043032a6ceed4184c89ac11bb6891abea6a7e9
JindeShubhamA <s.jinde@student.rug.nl>
2020-02-12 11:53:20 +0100

    Updated the start script

--- ModelsPython/start.sh
+++ ModelsPython/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
-#SBATCH --job-name=cnn-2
+#SBATCH --time=24:00:00
+#SBATCH --job-name=finetuning
 #SBATCH --mail-type=END,FAILL
 #SBATCH --mail-user=s.jinde@student.rug.nl
 #SBATCH --output=job-cnn-%j.log
@@ -9,6 +9,5 @@
 #SBATCH --ntasks=12
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-module load Python/3.7.2-GCCcore-8.2.0
-module load TensorFlow/2.0.0-foss-2019a-Python-3.7.2
-python dl_resnet_50.py
+module load TensorFlow/2.1.0-fosscuda-2019b-Python-3.7.4
+python dl_inception_finetuning.py
